75393984e052b2295f506bf0aef9db6a505c0a2c
rmzmiguel <miguel.ramirez9916@gmail.com>
2025-08-02 18:15:04 -0600

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default } from './FancyButton';
-export { default as FancyButton } from './FancyButton';
-export type { FancyButtonProps } from './FancyButton';
-export type { Variant, Appearance, Size } from './FancyButton';
-//# sourceMappingURL=index.d.ts.map
